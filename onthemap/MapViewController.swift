8fca63fe860c8d4dc52b411c9f3b2661e746788e
Matt Rocco <roccoma504@gmail.com>
2015-11-25 17:07:41 -0500

    added custom pins

--- onthemap/MapViewController.swift
+++ onthemap/MapViewController.swift
@@ -19,6 +19,16 @@ class MapViewController: UIViewController, MKMapViewDelegate{
         mapView.delegate = self
     }
     
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        // Set up the custom bins. Add an animation, the callout, and a button.
+        let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+        pin.animatesDrop = true
+        pin.canShowCallout = true
+        let btn = UIButton(type: .DetailDisclosure)
+        pin.rightCalloutAccessoryView = btn
+        return pin
+    }
+    
     // This funtion will start the JSON processing.
     func retrieveUserData() {
         // Wait for the JSON parsing to be comple via the completion
@@ -32,19 +42,22 @@ class MapViewController: UIViewController, MKMapViewDelegate{
     
     // This function will form the pins for display on the map.
     func formPins (infoArray : Array <StudentInformation>) {
-        print("form pin " + String(infoArray.count))
-        print("form pin " + String(infoArray[0]))
         
         // Loop around every student in the array and place their pin.
         for i in 0...infoArray.count - 1 {
             let newStudentPin = StudentPin(coordinate: infoArray[i].getLocaton(),
                 title: infoArray[i].getName(), subtitle: infoArray[i].getLink())
             mapView.addAnnotation(newStudentPin)
+            print(i)
         }
+    }
+    
+    func mapView(mapView: MKMapView, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        print("pin touched")
     }
     
     @IBAction func refresh(sender: AnyObject) {
         retrieveUserData()
     }
-
+    
 }
