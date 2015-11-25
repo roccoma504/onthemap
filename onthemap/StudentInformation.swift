b8562ff24ea5bdc32ba5f7e61cc9d23335ea658f
Matt Rocco <roccoma504@gmail.com>
2015-11-25 16:15:45 -0500

    travis

--- onthemap/StudentInformation.swift
+++ onthemap/StudentInformation.swift
@@ -10,7 +10,7 @@ import Foundation
 import CoreLocation
 import MapKit
 
-struct StudentInformation  {
+struct StudentInformation {
     
     var studentDict : Dictionary <String, AnyObject>
     
---
+++ onthemap/StudentPin.swift
@@ -0,0 +1,12 @@
+//
+//  StudentPin.swift
+//  onthemap
+//
+//  Created by Matthew Rocco on 11/25/15.
+//  Copyright © 2015 Matthew Rocco. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class StudentPin  {}
