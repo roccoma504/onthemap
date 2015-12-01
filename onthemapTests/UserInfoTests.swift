0ea8a48e0a816cec9d4cb898a511588765fbc8dc
Matt Rocco <roccoma504@gmail.com>
2015-11-30 20:58:42 -0500

    update tests

---
+++ onthemapTests/UserInfoTests.swift
@@ -0,0 +1,23 @@
+//
+//  UserInfoTests.swift
+//  onthemap
+//
+//  Created by Matthew Rocco on 11/30/15.
+//  Copyright © 2015 Matthew Rocco. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import onthemap
+
+class UserInfoTests : XCTestCase {
+    
+    func testGetters() {
+    var testUser = UserInfo()
+    testUser.setUserInfo("first", lastName: "last", ID: "1234")
+        XCTAssertEqual("first", testUser.getFirstName())
+    }
+}
+
+
+
