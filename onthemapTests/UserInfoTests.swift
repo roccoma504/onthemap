0f796605f49df4ffb991bf670585e3a7888c8251
Matt Rocco <roccoma504@gmail.com>
2015-12-02 19:43:07 -0500

    more tests

--- onthemapTests/UserInfoTests.swift
+++ onthemapTests/UserInfoTests.swift
@@ -16,6 +16,8 @@ class UserInfoTests : XCTestCase {
     var testUser = UserInfo()
     testUser.setUserInfo("first", lastName: "last", ID: "1234")
         XCTAssertEqual("first", testUser.getFirstName())
+        XCTAssertEqual("last", testUser.getLastName())
+        XCTAssertEqual("1234", testUser.getID())
     }
 }
 
