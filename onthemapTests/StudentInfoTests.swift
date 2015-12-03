a842e2c295e4cb6774e954ca95aa9333a24dc439
Matt Rocco <roccoma504@gmail.com>
2015-12-02 20:12:18 -0500

    student info tests

--- onthemapTests/StudentInfoTests.swift
+++ onthemapTests/StudentInfoTests.swift
@@ -8,17 +8,28 @@
 
 import Foundation
 import XCTest
+import MapKit
 @testable import onthemap
 
 class StudentInfoTests : XCTestCase {
     
     func testGetters() {
         
+        let location = CLLocationCoordinate2D(latitude: 0.0, longitude : 0.0)
+        
         var testDirectory = [String: AnyObject]()
         testDirectory["firstName"] = "first"
         testDirectory["lastName"] = "last"
-        
+        testDirectory["mediaURL"] = "www.google.com"
+        testDirectory["latitude"] = location.latitude
+        testDirectory["longitude"] = location.longitude
+
         let studentInfo = StudentInformation(studentDict: testDirectory)
         XCTAssertEqual("first last", studentInfo.getName())
+        XCTAssertEqual("www.google.com", studentInfo.getLink())
+        XCTAssertEqual(location.latitude, studentInfo.getLocaton().latitude)
+        XCTAssertEqual(location.longitude, studentInfo.getLocaton().longitude)
+
+
     }
 }
