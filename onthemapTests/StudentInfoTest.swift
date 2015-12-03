78e756e9664552677f2d2ac57a43afe207f47e2e
Matt Rocco <roccoma504@gmail.com>
2015-12-02 19:45:21 -0500

    coverage and more tests

--- onthemapTests/StudentInfoTest.swift
+++
@@ -1,9 +0,0 @@
-//
-//  StudentInfoTest.swift
-//  onthemap
-//
-//  Created by Matthew Rocco on 12/2/15.
-//  Copyright © 2015 Matthew Rocco. All rights reserved.
-//
-
-import Foundation
---
+++ onthemapTests/StudentInfoTests.swift
@@ -0,0 +1,22 @@
+//
+//  StudentInfoTest.swift
+//  onthemap
+//
+//  Created by Matthew Rocco on 12/2/15.
+//  Copyright © 2015 Matthew Rocco. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import onthemap
+
+class StudentInfoTests : XCTestCase {
+    
+    func testGetters() {
+        
+        let testDirectory =
+
+        
+        var studentInfo = StudentInformation(studentDict: <#T##Dictionary<String, AnyObject>#>)
+    }
+}
